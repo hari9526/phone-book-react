a402db7a6d0b0c7a26aa5dacd42c8eb8d756609c
Harikrishnan R <hari04.krishnan@gmail.com>
2022-01-26 17:25:09 +0530

    Created a new component for subscribers and css changes

---
+++ src/AddSubscriber.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import Header from './Header';
+
+class AddSubscriber extends Component {
+    render() {
+        return (
+            <div>
+                <Header heading="Add Subscriber" />
+                <div className="component-body-container">
+                    <button className="custom-btn">Back</button>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default AddSubscriber; 
