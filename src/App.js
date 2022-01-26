2e27e1ab449c17f295fff732cc6c07f76ce9a0d9
Harikrishnan R <hari04.krishnan@gmail.com>
2022-01-26 16:49:01 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Header from './Header.js';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  deleteHandler(msg) {
+    alert(msg);
+  }
+
+  render() {
+    let subscribers = [
+      {
+        name: 'Hari',
+        number: 123123123123,
+      },
+      {
+        name: 'Disny',
+        number: 123123123123,
+      },
+    ];
+    return (
+      <div>
+        <Header heading="Phone Directory" />
+        <div className="component-body-container">
+          <button className="custom-btn add-btn">Add</button>
+          <div className="grid-container heading-container">
+            <span className="grid-item name-heading">Name</span>
+            <span className="grid-item phone-heading">Phone</span>
+          </div>
+
+          {subscribers.map((sub, index) => {
+            return (
+              <div className="grid-container" key={index}>
+                <span className="grid-item">{sub.name}</span>
+                <span className="grid-item">{sub.number}</span>
+                <span className="grid-item">
+                  <button type="button" className="custom-btn delete-btn" onClick={this.deleteHandler.bind(this, "Delete clicked")}>Delete</button>
+                </span>
+
+              </div>
+            )
+          })}
+        </div>
+      </div>
+    );
+  }
 }
 
-export default App;
+
+export default App; 
